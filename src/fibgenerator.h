31ecc1fdc9c495799215c2420b664672671731b8
zj27 <zj27jk@163.com>
2016-05-10 21:58:21 +0800

    Update the h files

--- src/fibgenerator.h
+++ src/fibgenerator.h
@@ -1,5 +1,10 @@
 #include <string>
 
+#ifndef FIB_GENERATOR_H
+#define FIB_GENERATOR_H 
+
 const int MAX_NUM = 94;
 
 std::string generate_fibnum_json(const unsigned int num);
+
+#endif
--- src/fibservice.h
+++ src/fibservice.h
@@ -1,2 +1,6 @@
+#ifndef FIB_SERVICE_H
+#define FIB_SERVICE_H 
 
 void start_fibservice();
+
+#endif
